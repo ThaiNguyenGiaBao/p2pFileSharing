1da04d77bd4aee7302f4aac7cb50d053da087886
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-10-29 20:11:37 +0700

    ok

--- src/tracker/router/piece.router.ts
+++ src/tracker/router/piece.router.ts
@@ -5,10 +5,16 @@ import PieceController from "../controller/piece.controller";
 const router = Router();
 router.post("/register", asyncHandler(PieceController.registerPiece));
 
+// Get all pieces by torrentId
+router.get(
+  "/:torrentId",
+  asyncHandler(PieceController.getPiecesByTorrentId)
+);
 
 //Get all peers by hashPiece
-router.get("/peer/:hashPiece", asyncHandler(PieceController.getPeersByHashPiece));
-
-
+router.get(
+  "/peer/:hashPiece",
+  asyncHandler(PieceController.getPeersByHashPiece)
+);
 
 export default router;
