a3003cd90e6fb078f8e86f184e0f92584fc3efe6
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-11-08 10:55:44 +0700

    multithread downloadFile

--- src/peer/peerClient.ts
+++ src/peer/peerClient.ts
@@ -4,7 +4,6 @@ import axios from "axios";
 import path from "path";
 import { Peer, Piece } from "../types";
 import { saveFilePiece } from "./filePiecesManager"; // Import các hàm cần thiết
-import ProgressBar from "progress";
 import { Worker } from "worker_threads";
 
 // Hàm tải xuống một phần (piece) của tệp từ một peer
@@ -13,14 +12,14 @@ const downloadPieceFromPeer = async (
   pieceIndex: number,
   filename: string,
   filePath: string
-): Promise<{ data: any; isSuccess: boolean }> => {
+): Promise<boolean> => {
   return new Promise((resolve) => {
     const client = net.createConnection(
       { port: peer.port, host: peer.ip },
       () => {
-        // console.log(
-        //   `Connected to peer ip:${peer.ip}, port:${peer.port} to download #${pieceIndex}`
-        // );
+        console.log(
+          `Connected to peer ip:${peer.ip}, port:${peer.port} to download piece ${pieceIndex}`
+        );
 
         // Yêu cầu peer gửi phần của tệp
         client.write(
@@ -34,11 +33,12 @@ const downloadPieceFromPeer = async (
           if (message.startsWith("ERROR:")) {
             console.error(`Received error from donwloaded peer: ${message}`);
             // Trả về false khi có lỗi
-            resolve({ data: null, isSuccess: false });
+            resolve(false);
             return; // Thoát khỏi callback
           }
 
-          resolve({ data, isSuccess: true });
+          // Trả về true khi tải xuống thành công
+          resolve(true);
           client.end();
         });
       }
@@ -48,7 +48,7 @@ const downloadPieceFromPeer = async (
       console.log(
         `Error connecting to peer ip:${peer.ip}, port:${peer.port}: ${err.message}`
       );
-      resolve({ data: null, isSuccess: false }); // Trả về false khi có lỗi kết nối
+      resolve(false); // Trả về false khi có lỗi kết nối
     });
   });
 };
@@ -80,20 +80,10 @@ const downloadFile = async (filename: string, myPeer: Peer) => {
 
     console.log(`Found ${pieces.length} pieces for the file ${filename}.`);
 
-    const bar = new ProgressBar(
-      "[:bar] :percent Downloaded piece #:idx with size :size Bytes from peer ip: :ip, port: :port successfully",
-      {
-        total: pieces.length,
-        width: 20,
-        complete: "#",
-        incomplete: "-",
-      }
-    );
-
-    // Simulate a process with a timer
-
-    const pieceDataList = Array(pieces.length);
     const workers: Worker[] = [];
+
+    // Declare a list to store the downloaded piece data with the size = pieces.length
+    const pieceFileDataList: any[] = new Array(pieces.length);
 
     // Tải xuống từng phần của tệp từ các peer
     for (let pieceIndex = 0; pieceIndex < pieces.length; pieceIndex++) {
@@ -104,34 +94,29 @@ const downloadFile = async (filename: string, myPeer: Peer) => {
           filename,
           filePath,
           pieceIndex,
-          bar,
         },
       });
-
       worker.on("message", (message) => {
         if (message.success) {
-          console.log(`Successfully downloaded piece #${message.pieceIndex}`);
-
-          pieceDataList[message.pieceIndex] = message.data;
+          console.log(
+            `Download piece #${message.pieceIndex} with size ${message.data.length} Bytes from peer ip:${message.ip} port:${message.port} successfully!`
+          );
+        
+          pieceFileDataList[message.pieceIndex] = message.data;
         } else {
           console.log(`Failed to download piece #${message.pieceIndex}`);
         }
       });
-
       worker.on("error", (error) => {
         console.error("Worker error:", error);
       });
-
       worker.on("exit", (code) => {
         if (code !== 0) {
           console.error(`Worker stopped with exit code ${code}`);
         }
       });
-
       workers.push(worker);
     }
-
-    // Ghi các phần đã tải xuống vào tệp
 
     // Wait for all workers to finish
     const workerPromises = workers.map((worker) => {
@@ -144,14 +129,16 @@ const downloadFile = async (filename: string, myPeer: Peer) => {
 
     await Promise.all(workerPromises);
 
-    for (let i = 0; i < pieceDataList.length; i++) {
-      if (pieceDataList[i]) {
-        //console.log(`Writing #${i} to file`);
-        fs.appendFileSync(filePath, pieceDataList[i], { flag: "a" });
-        saveFilePiece(filePath, i, pieceDataList[i]); // Cập nhật danh sách các phần
-      }
+    //console.log("All workers finished downloading pieces");
+
+    // // Write the downloaded piece data to the file
+    for (let i = 0; i < pieceFileDataList.length; i++) {
+      //console.log(`#${i} has size: ${pieceFileDataList[i].length}`);
+      fs.appendFileSync(filePath, pieceFileDataList[i], { flag: "a" });
+
+      saveFilePiece(filePath, i, pieceFileDataList[i]);
     }
-    console.log(`Downloaded file ${filename} successfully!`);
+    console.log(`File ${filename} has been downloaded successfully!`);
   } catch (err: any) {
     console.log("Error downloading file:", err.message);
   }
